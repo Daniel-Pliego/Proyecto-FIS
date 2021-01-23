1fb8ab18691715348010e18a1064d261e484548f
Charly-Sz18 <carlosfsanchezchuc@gmail.com>
2021-01-23 16:11:21 -0700

    codificacion de las preguntas  para el skill

---
+++ Codificación/codificacion.(preguntas-listas)/lambda/index.js
@@ -0,0 +1,397 @@
+/*
+ * Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+// sets up dependencies
+const Alexa = require('ask-sdk-core');
+const i18n = require('i18next');
+const languageStrings = require('./languageStrings');
+
+// core functionality for fact skill
+const GetFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    // checks request type
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetFactIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    // gets a random fact by assigning an array to the variable
+    // the random item from the array will be selected by the i18next library
+    // the i18next library is set up in the Request Interceptor
+    const randomFact = requestAttributes.t('FACTS');
+    // concatenates a standard message with the random fact
+    const speakOutput = requestAttributes.t('GET_FACT_MESSAGE') + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speakOutput)
+      // Uncomment the next line if you want to keep the session open so you can
+      // ask for another fact without first re-opening the skill
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .withSimpleCard(requestAttributes.t('SKILL_NAME'), randomFact)
+      .getResponse();
+  },
+};
+
+
+
+
+
+
+
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type ===  'LaunchRequest';
+    },
+    handle(handlerInput) {
+        return GetFactIntentHandler.handle(handlerInput);
+    }
+};
+
+const GetFactIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'GetFactIntent'
+            || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('FACTS');
+        return handlerInput.responseBuilder
+            .speak(speechText + "     " + requestAttributes.t('HELP_REPROMPT'))
+            .reprompt(requestAttributes.t('HELP_REPROMPT')) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+const doubtIntenthandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+        && handlerInput.requestEnvelope.request.intent.name === 'doubtIntent'
+    },
+       handle(handlerInput) {
+           const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+           const speechText= requestAttributes.t('DOUBT_MSG');
+         
+           return handlerInput.responseBuilder
+               .speak(speechText)
+               .reprompt(speechText)
+               .withSimpleCard(requestAttributes.t('SKILL_NAME'),speechText)
+               .getResponse();
+    }
+};
+
+
+const QuestiononeIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name=== 'QuestiononeIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+const QuestiontwoIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestiontwoIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER2');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+const QuestionthreeIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestionthreeIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER3');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+
+const QuestionfourIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestionfourIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER4');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+const QuestionfiveIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestionfiveIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER5');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+const QuestionsisIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestionsisIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER6');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+const QuestionsevenIntentHandler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'QuestionsevenIntent'
+           // || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.YesIntent'   // <-- necessary so the user gets another fact when it says 'yes' when asked if they want another one
+            //|| handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NextIntent');// <-- necessary so the gets another fact when it says 'next' (you might want to extend the utterances of this intent with e.g. 'next fact' and similar)
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('ANSWER7');
+        
+        return handlerInput.responseBuilder
+            .speak(speechText )
+            .reprompt(speechText) // <-- necessary to keep the session open so users can request another fact
+            .withSimpleCard(requestAttributes.t('SKILL_NAME'), speechText)
+            .getResponse();
+    }
+};
+
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.NoIntent'); // <-- necessary so the session is closed when the user doesn't want more facts
+    },
+    handle(handlerInput) {
+        const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+        const speechText = requestAttributes.t('STOP_MESSAGE');
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .getResponse();
+    }
+};
+
+
+
+
+
+
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('HELP_MESSAGE'))
+      .reprompt(requestAttributes.t('HELP_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const FallbackHandler = {
+  // The FallbackIntent can only be sent in those locales which support it,
+  // so this handler will always be skipped in locales where it is not supported.
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.FallbackIntent';
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('FALLBACK_MESSAGE'))
+      .reprompt(requestAttributes.t('FALLBACK_REPROMPT'))
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('STOP_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+    console.log(`Error stack: ${error.stack}`);
+    const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
+    return handlerInput.responseBuilder
+      .speak(requestAttributes.t('ERROR_MESSAGE'))
+      .reprompt(requestAttributes.t('ERROR_MESSAGE'))
+      .getResponse();
+  },
+};
+
+const LocalizationInterceptor = {
+  process(handlerInput) {
+    // Gets the locale from the request and initializes i18next.
+    const localizationClient = i18n.init({
+      lng: handlerInput.requestEnvelope.request.locale,
+      resources: languageStrings,
+      returnObjects: true
+    });
+    // Creates a localize function to support arguments.
+    localizationClient.localize = function localize() {
+      // gets arguments through and passes them to
+      // i18next using sprintf to replace string placeholders
+      // with arguments.
+      const args = arguments;
+      const value = i18n.t(...args);
+      // If an array is used then a random value is selected
+      if (Array.isArray(value)) {
+        return value[Math.floor(Math.random() * value.length)];
+      }
+      return value;
+    };
+    // this gets the request attributes and save the localize function inside
+    // it to be used in a handler by calling requestAttributes.t(STRING_ID, [args...])
+    const attributes = handlerInput.attributesManager.getRequestAttributes();
+    attributes.t = function translate(...args) {
+      return localizationClient.localize(...args);
+    }
+  }
+};
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+   // GetFactHandler,
+    LaunchRequestHandler,
+    GetFactIntentHandler,
+    doubtIntenthandler,
+    QuestiononeIntentHandler,
+    QuestiontwoIntentHandler,
+    QuestionthreeIntentHandler,
+    QuestionfourIntentHandler,
+    QuestionfiveIntentHandler,
+    QuestionsisIntentHandler,
+    QuestionsevenIntentHandler,
+    HelpHandler,
+    CancelAndStopIntentHandler,
+    //ExitHandler,
+    FallbackHandler,
+    SessionEndedRequestHandler,
+  )
+  .addRequestInterceptors(LocalizationInterceptor)
+  .addErrorHandlers(ErrorHandler)
+  .withCustomUserAgent('sample/basic-fact/v2')
+  .lambda();
